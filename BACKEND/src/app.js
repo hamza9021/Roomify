e47ce229320a13865e14f808d6f1ab9508c226cb
Hamza Riaz <hriaz9803@gmail.com>
2025-05-08 10:10:41 +0500

    fix: Enhance CORS configuration by adding dynamic headers for credentials and origin

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -17,9 +17,15 @@ const corsOptions = {
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 
-
 app.use(cors(corsOptions));
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  next();
+});
 
 app.options("*", cors(corsOptions)); // Preflight response
 
