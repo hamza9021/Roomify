8c1032fb7509469064b8471245e38e4e3703ef9c
Hamza Riaz <hriaz9803@gmail.com>
2025-05-08 08:51:23 +0500

    fix: Remove domain option from cookie settings in loginUser

--- BACKEND/src/Controllers/user.controllers.js
+++ BACKEND/src/Controllers/user.controllers.js
@@ -77,7 +77,6 @@ const loginUser = wrapperFunction(async (req, res) => {
         httpOnly: true,
         secure: true,
         sameSite: "none",
-        domain: "roomify-drab",
         maxAge: 7 * 24 * 60 * 60 * 1000,
     };
 
