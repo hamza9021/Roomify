3fa84c3191ec1a580e1702aae362f8460e24b769
Hamza Riaz <hriaz9803@gmail.com>
2025-05-13 00:17:26 +0500

    fix: Simplify vercel.json by removing unnecessary routes and headers

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
         target: 'https://roomify-2-2y0a.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
-        secure: true // Only if your backend doesn't have proper SSL
+        secure: true
       }
     }
   }
