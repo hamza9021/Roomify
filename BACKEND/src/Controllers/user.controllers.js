0d25aa90bb75e52369b6f56455dceee63a2344d5
Muhammad Bilal Ellahi <bilalillahi25@gmail.com>
2025-07-16 08:12:09 +0500

    testing priority

--- BACKEND/src/Controllers/user.controllers.js
+++ BACKEND/src/Controllers/user.controllers.js
@@ -76,8 +76,11 @@ const loginUser = wrapperFunction(async (req, res) => {
     const cookieOptions = {
         httpOnly: true,
         secure: true,
-        sameSite: process.env.NODE_ENV === "production" && "lax",
+        // sameSite: process.env.NODE_ENV === "production" && "lax",
         maxAge: 24 * 60 * 60 * 1000, 
+        
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  priority: 'high'
         // domain: ".onrender.com", 
         // path: "/",
     };
