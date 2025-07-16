dca97a2a516336c102b19333613885bbdc5aa5b7
Muhammad Bilal Ellahi <bilalillahi25@gmail.com>
2025-07-16 06:37:24 +0500

    path

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -10,7 +10,7 @@ import { listingRouter } from "./Routes/listing.routes.js";
 import { reviewRouter } from "./Routes/review.routes.js";
 import { messageRouter } from "./Routes/message.routes.js";
 import { authRouter } from "./Routes/auth.routes.js";
-import { wishListRouter } from "./Routes/wishlist.routes.js";
+import { wishListRouter } from "./Routes/wishlist.route.js";
 import { bookingRouter } from "./Routes/booking.routes.js";
 import "./Services/passport.js";
 
