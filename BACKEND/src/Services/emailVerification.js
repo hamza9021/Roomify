df4fe690ace31ccb31af59a148559f5bb3b3c6d5
Hamza Riaz <hriaz9803@gmail.com>
2025-11-22 15:45:29 -0800

    feat: add Nodemailer transporter setup for email verification

--- BACKEND/src/Services/emailVerification.js
+++ BACKEND/src/Services/emailVerification.js
@@ -2,8 +2,8 @@ import nodemailer from "nodemailer";
 // Create a test account or replace with real credentials.
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // true for 465, false for other ports
+    port: 465,
+    secure: true, // true for 465, false for other ports
     auth: {
         user: `${process.env.AUTH_EMAIL}`,
         pass: `${process.env.AUTH_PASS}`,
